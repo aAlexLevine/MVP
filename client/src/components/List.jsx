c5f6cef099c4846326dfc6180bb827eeb6e84887
Alex Levine <alexlevine@Alexs-MacBook-Pro-2.local>
2018-02-17 15:00:10 -0500

    changes for deployment to heroku

--- client/src/components/List.jsx
+++ client/src/components/List.jsx
@@ -10,7 +10,7 @@ const List = (props) => {
   return (<div style ={simStyle}>
     <h4> Similar Artists </h4>
     
-    <h5>Here are the top { props.artists.length} similar artists to {props.artistName}.</h5>
+    <h5>Here are the top { props.artists.length } similar artists to {props.artistName}.</h5>
     
     { props.artists.map((artist, idx) => <ListItem singleArtist={artist.name} matchPercentage={artist.match} artistImg={artist.image} key={idx}/>)}
   </div>)
--- client/src/components/ListItem.jsx
+++ client/src/components/ListItem.jsx
@@ -12,8 +12,12 @@ const clickStyle = {
  };
 
  const nameStyle = {
-  fontWeight: 'bold'
+  fontWeight: 'bold',
+  fontSize: '20px'
  };
+
+
+
 
 class ListItem extends React.Component {
   constructor(props) {
@@ -22,7 +26,8 @@ class ListItem extends React.Component {
       descrip: false,
       clickedOnArtists: '',
       displayTracks: false,
-      tracks: []
+      tracks: [],
+     
     }
     console.log(this.props.artistImg) 
   }
@@ -38,16 +43,28 @@ class ListItem extends React.Component {
     })
   }
 
+  // green() {
+  //   this.setState({color:{color:'green'}})
+  // }
+  // yellow() {
+  //   this.setState({color:{color:'yellow'}})
+  // }
 
+  // {this.props.matchPercentage > .70 ? this.setState({color: 'green'}) :null}
+  // {this.props.matchPercentage > .70 ? this.green() : this.yellow()} 
   
   render () {
     
     return (
       
-      <div style={divStyle} onClick={() => this.setState({descrip: !this.state.descrip, clickedOnArtists: this.props.singleArtist })}>
-        
-        <div style={nameStyle}>{this.props.singleArtist}</div> <br/>This artist shares a {(this.props.matchPercentage * 100).toFixed(2)}% similarity. 
-        
+      
+      <div  style={divStyle} >
+      
+        <div style={nameStyle} onClick={() => this.setState({descrip: !this.state.descrip, clickedOnArtists: this.props.singleArtist })}>{this.props.singleArtist}</div> 
+
+ 
+        <div > <br/>This artist shares a {(this.props.matchPercentage * 100).toFixed(2)}% similarity. </div>
+      
         {this.state.descrip ? 
           <div onClick={(e) => {e.stopPropagation();  console.log('inner click, the subArtist clicked is ', this.state.clickedOnArtists), this.setState({displayTracks: !this.state.displayTracks}), this.getThisArtistsTracks()   } }> 
             <br/> 
