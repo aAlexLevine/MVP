c2e038428ca8d81f33246db08118fc504fd39b04
Alex Levine <alexlevine@Alexs-MacBook-Pro-2.local>
2018-02-17 15:12:55 -0500

    heroku token

--- server/lastFM.js
+++ server/lastFM.js
@@ -1,5 +1,5 @@
 var request = require('request')
-let config = require('../config.js').TOKEN;
+let config = process.env.GITHUB_TOKEN || require('../config.js').TOKEN;
 
 getNewArtistsByArtist = (artist, callback) => {
     let options = {
